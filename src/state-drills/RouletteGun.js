169a4bacaff5a73c527d63b8ec127edcf3606a0d
Yoseph Student <blsck1024@gmail.com>
2020-01-02 13:15:56 -0600

    finished!!

--- src/state-drills/RouletteGun.js
+++ src/state-drills/RouletteGun.js
@@ -3,28 +3,53 @@ import React from 'react';
 class RouletteGun extends React.Component{
   static defaultProps = {
     bulletInTheChamber : 8,
-  }
+  };
+  
 
   state = {
     chamber: null,
     spinningTheChamber: false,
-  }
+  };
+  
 
   handleClick = () => {
     this.setState({
       spinningTheChamber : true
     })
-    let timeout = setTimeout()
+
+    this.timeout = setTimeout(() => {
+      let randomChamber = Math.ceil(Math.random() * 8)
+
+    this.setState({
+      chamber: randomChamber,
+      spinningTheChamber: false,
+    })
+    }, 2000)
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeout)
+  }
+
+  renderDisplay() {
+    const {chamber, spinningTheChamber } = this.state
+    const { bulletInTheChamber } = this.props
+    if (spinningTheChamber) {
+      return 'spinning the chamber and pulling the trigger! ...'
+    } else if (chamber === bulletInTheChamber) {
+      return 'BANG!!!!'
+    } else {
+      return 'you\'re safe!'
+    }
   }
 
   render(){
     return (
-      <div>
-          <button>Pull the trigger!</button>
-          {<p>spinning the chamber and pulling the trigger! ...</p>}
-          {<p>you're safe!</p>}
-          {<p>BANG!!!</p>}
-
+      <div className='RouletteGun'>
+          <p>{this.renderDisplay()}</p>
+          <button onClick={this.handleClick}>
+            Pull the trigger!
+          </button>
       </div>
     )
   }
